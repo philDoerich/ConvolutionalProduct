ca5056c861f08a2edeb7f1b9e34501cee1983e43
Phil Dörich <doerich.phil@gmail.com>
2020-09-04 10:47:49 +0200

    Add static build methods, add getter for coefficients

--- convolutionalProduct/src/polynomial/Polynomial.java
+++ convolutionalProduct/src/polynomial/Polynomial.java
@@ -1,5 +1,27 @@
 package polynomial;
 
-public class Polynomial {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Polynomial {
+	
+	private final ArrayList<Double> coefficients;
+	
+	private Polynomial(ArrayList<Double> coeffs) {
+		this.coefficients = coeffs;
+	}
+	
+	public static Polynomial buildFromCoefficients(ArrayList<Double> coeffs) {
+		return new Polynomial(coeffs);
+	}
+	
+	public static Polynomial buildZeroPolynomial() {
+		ArrayList<Double> zeroList = new ArrayList<>();
+		zeroList.add(Double.valueOf(0));
+		return new Polynomial(zeroList);
+	}
+	
+	public List<Double> getCoefficients(){
+		return coefficients;
+	}
 }
